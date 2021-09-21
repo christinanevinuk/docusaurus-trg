e5bc965952d90aa5142324aa57ce7cb9a9a2f315
Christina Nevin <christinanevinuk@gmail.com>
2021-09-21 17:36:04 +0100

    buttons

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -64,9 +64,9 @@ class HomeSplash extends React.Component {
         <div className="inner">
           <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
           <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
+            <Button href="#try">Neverwhere</Button>
+            <Button href={docUrl('doc1.html')}>TRG</Button>
+            <Button href={docUrl('doc2.html')}>Elsewhere</Button>
           </PromoSection>
         </div>
       </SplashContainer>
